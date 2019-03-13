ef7a44dc3381fee0ab7eef626e4bfb414a8e9655
sohil <sohils413@gmail.com>
2019-03-13 10:48:39 +0530

    first

---
+++ html5_video.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<video controls autoplay src="http://media.w3.org/2010/05/sintel/trailer.webm">
+		<track default src="sample.vtt" kind="subtitles" srclang="en" label="English">
+	</video>
+</body>
+</html>
